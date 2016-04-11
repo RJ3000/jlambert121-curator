dcbbba477bded9c91ed3882b2228116fcb3002bc
Richard Julier <richard.julier@bedegaming.com>
2016-04-11 21:06:57 +0100

    Adding support for cronic

--- manifests/job.pp
+++ manifests/job.pp
@@ -8,6 +8,7 @@ define curator::job (
   $ensure                = 'present',
   $sub_command           = 'indices',
   $bin_file              = $::curator::bin_file,
+  $cronic_cin            = $::curator::cronic_bin,
 
   # ES config
   $host                  = $::curator::host,
